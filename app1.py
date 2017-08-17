9dbd1155b065c464f257e070cda5598f7152d382
Lena <Lwa3000@gmail.com>
2017-08-16 21:54:17 -0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,5 +1,5 @@
 '''
-Description: A program that gives to the definition of a word.
+Description: A program that outputs the definition of a word.
 Features: uses python command lines,
 Date: Aug 13,2017
 Name: LenC
